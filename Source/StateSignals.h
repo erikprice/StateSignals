b6fab085cdaac913614092cb16a973eb9908458d
Erik Price <mail@erikprice.com>
2013-08-14 07:53:03 -0400

    Initial Xcode project

---
+++ Source/StateSignals.h
@@ -0,0 +1,13 @@
+//
+//  StateSignals.h
+//  StateSignals
+//
+//  Created by Erik Price on 2013-08-14.
+//  Copyright (c) 2013 Erik Price. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StateSignals : NSObject
+
+@end
---
+++ Source/StateSignals.m
@@ -0,0 +1,13 @@
+//
+//  StateSignals.m
+//  StateSignals
+//
+//  Created by Erik Price on 2013-08-14.
+//  Copyright (c) 2013 Erik Price. All rights reserved.
+//
+
+#import "StateSignals.h"
+
+@implementation StateSignals
+
+@end
