205aee8c5560bf7199399596e87d9e553e23c789
Erik Price <mail@erikprice.com>
2013-08-14 09:47:53 -0400

    Signal machine interface

--- Source/CPBSignalMachine.h
+++ Source/CPBSignalMachine.h
@@ -8,12 +8,59 @@
 
 #import <Foundation/Foundation.h>
 
-#import "CPBTransitionTable.h"
+@class CPBTransitionTable;
 
 
 @interface CPBSignalMachine : NSObject
 
+/// Creates a new signal machine with the specified transition table.
+///
+/// @param table The transition table to be used by this signal machine. A defensive copy is made.
+/// @return The newly-created signal machine.
 - (id)initWithTransitionTable:(CPBTransitionTable *)table;
 
+/// Notifies this signal machine of a new input event, to prompt a transition.
+///
+/// @param event The input event occurring in this instance.
+- (void)inputEvent:(NSString *)event;
+
+/// Notifies this signal machine of a new input event with the specified event context, to prompt a transition.
+///
+/// @param event The input event occurring in this instance.
+/// @param eventContext A context object to be included as payload to transition handlers.
+- (void)inputEvent:(NSString *)event context:(id)eventContext;
+
+/// Returns a new signal which sends every event received by this signal machine.
+///
+/// @return a new signal which sends every event received by this signal machine.
+- (RACSignal *)allEvents;
+
+/// Returns a new signal which sends every transition made by this signal machine.
+/// Sends an error if any event is sent which has no corresponding transition mapping.
+///
+/// @return a new signal which sends every transition made by this signal machine.
+- (RACSignal *)allTransitions;
+
+/// Returns a new signal which sends every transition made by this signal machine from the specified state.
+/// Sends an error if any event is sent which has no corresponding transition mapping.
+///
+/// @param fromState The "from state" from which transitions must be made to be sent on the returned signal.
+/// @return a new signal which sends every transition made by this signal machine from the specified state.
+- (RACSignal *)transitionsFrom:(NSString *)fromState;
+
+/// Returns a new signal which sends every transition made by this signal machine from the specified `fromState` to the specified `toState`.
+/// Sends an error if any event is sent which has no corresponding transition mapping.
+///
+/// @param fromState The "from state" from which transitions must be made to be sent on the returned signal.
+/// @param toState The "to state" to which transitions must be made to be sent on the returned signal.
+/// @return a new signal which sends every transition made by this signal machine from the specified `fromState` to the specified `toState`.
+- (RACSignal *)transitionsFrom:(NSString *)fromState to:(NSString *)toState;
+
+/// Returns a new signal which sends every transition made by this signal machine to the specified state.
+/// Sends an error if any event is sent which has no corresponding transition mapping.
+///
+/// @param toState The "to state" to which transitions must be made to be sent on the returned signal.
+/// @return a new signal which sends every transition made by this signal machine to the specified state.
+- (RACSignal *)transitionsTo:(NSString *)toState;
 
 @end
--- Source/CPBSignalMachine.m
+++ Source/CPBSignalMachine.m
@@ -8,6 +8,67 @@
 
 #import "CPBSignalMachine.h"
 
+#import "CPBTransitionTable.h"
+
+
+@interface CPBSignalMachine ()
+
+@property (nonatomic, strong) CPBTransitionTable *transitionTable;
+
+@end
+
+
 @implementation CPBSignalMachine
+
+- (id)init
+{
+    NSLog(@"%@ is not a valid initializer; use -[CPBSignalMachine initWithTransitionTable:] instead", NSStringFromSelector(_cmd));
+    return [super init];
+}
+
+- (id)initWithTransitionTable:(CPBTransitionTable *)table
+{
+    if (self = [super init])
+    {
+        _transitionTable = [table copy];
+    }
+    
+    return self;
+}
+
+- (void)inputEvent:(NSString *)event
+{
+    
+}
+
+- (void)inputEvent:(NSString *)event context:(id)eventContext
+{
+    
+}
+
+- (RACSignal *)allEvents
+{
+    return nil;
+}
+
+- (RACSignal *)allTransitions
+{
+    return nil;
+}
+
+- (RACSignal *)transitionsFrom:(NSString *)fromState
+{
+    return nil;
+}
+
+- (RACSignal *)transitionsFrom:(NSString *)fromState to:(NSString *)toState
+{
+    return nil;
+}
+
+- (RACSignal *)transitionsTo:(NSString *)toState
+{
+    return nil;
+}
 
 @end
